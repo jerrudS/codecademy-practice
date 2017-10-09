32fc11cb74ce9a88480d2ccfd32c5069de17432d
Jerrud Sabourin <jsabourin1235@gmail.com>
2017-10-09 12:24:53 -0700

    Class extends, super, static.

--- classes/main.js
+++ classes/main.js
@@ -28,3 +28,49 @@ const surgeonDurant = new Surgeon('Durant', 'Orthopedics');
 console.log(surgeonCurry.name)
 surgeonCurry.takeVacationDays(3)
 console.log(surgeonCurry.remainingVacationDays)
+
+//class extends, super, static
+class HospitalEmployee {
+  constructor(name) {
+    this._name = name;
+    this._remainingVacationDays = 20;
+  }
+
+  static generatePassword() {
+    const randomInteger = Math.floor(Math.random() * 10000)
+    return randomInteger
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get remainingVacationDays() {
+    return this._remainingVacationDays;
+  }
+
+  takeVacationDays(daysOff) {
+    this._remainingVacationDays -= daysOff;
+  }
+}
+
+class Nurse extends HospitalEmployee {
+  constructor(name, certifications) {
+    super(name);
+    this._certifications = certifications;
+  }
+
+  get certifications() {
+    return this._certifications;
+  }
+
+  addCertification(newCertification) {
+    this.certifications.push(newCertification);
+  }
+}
+
+const nurseOlynyk = new Nurse('Olynyk', ['Trauma','Pediatrics']);
+nurseOlynyk.takeVacationDays(5);
+console.log(nurseOlynyk.remainingVacationDays);
+nurseOlynyk.addCertification('Genetics');
+console.log(nurseOlynyk.certifications);
