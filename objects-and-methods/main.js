da200e513e4fdd92807badfd9759aa4037d25323
Jerrud Sabourin <jsabourin1235@gmail.com>
2017-10-09 10:38:01 -0700

    Getters and setters.

--- objects-and-methods/main.js
+++ objects-and-methods/main.js
@@ -38,3 +38,23 @@ if (day === 'Saturday' || day === 'Sunday' ) {
 
 console.log(person[alarm]);
 console.log(friend.sayHello())
+
+// getters and setters
+let person = {
+  _name: 'Lu Xun',
+  _age: 137,
+ 	set age(newAge) {
+  	if(typeof newAge === 'number') {
+      this._age = newAge
+      console.log(`Age is now ${newAge}`)
+    }
+    else {
+      console.log('Invalid input')
+    }
+	},
+  get age() {
+    console.log(`${this._name} is ${this._age} years old.`)
+  }
+};
+
+console.log(person.age)
