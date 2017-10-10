eb327dfdd407cfae7ffd26f4c233558dc34338dd
Jerrud Sabourin <jsabourin1235@gmail.com>
2017-10-10 15:41:21 -0700

    Added AJAX POST refactored function.

--- requests/refactored-main.js
+++ requests/refactored-main.js
@@ -1,4 +1,4 @@
-// refactored expandUrl function to JQuery AJAX request format
+// refactored expandUrl function to JQuery AJAX GET request format
 
 function expandUrl() {
 	const urlToExpand = url + '?shortUrl=' + $inputField.val() + '&key=' + apiKey;
@@ -8,6 +8,25 @@ function expandUrl() {
     dataType: 'json',
     success(response) {
       $responseField.append('<p>Your expanded url is: </p><p>' + response.longUrl + '</p>');
+    },
+    error(jqXHR, status, errorThrown) {
+      console.log(jqXHR)
+    }
+  });
+}
+
+// refactored shortenUrl function to JQuery AJAX POST request format
+function shortenUrl() {
+	const urlWithKey = url + '?key=' + apiKey;
+  const urlToShorten = $inputField.val()
+  $.ajax({
+    url: urlWithKey,
+    type: 'POST',
+    data: JSON.stringify({longUrl: urlToShorten}),
+    dataType: 'json',
+    contentType: 'application/json',
+    success(response) {
+      $responseField.append('<p>Your shortened url is: </p><p>' + response.id + '</p>');
     },
     error(jqXHR, status, errorThrown) {
       console.log(jqXHR)
