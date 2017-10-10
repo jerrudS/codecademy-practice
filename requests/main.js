df1f0e10b1d4a4b2af37ae6153fe659b1575fe08
Jerrud Sabourin <jsabourin1235@gmail.com>
2017-10-10 13:34:17 -0700

    Another AJAX request example.

--- requests/main.js
+++ requests/main.js
@@ -9,3 +9,52 @@ xhr.onreadystatechange = function(){
 }
 xhr.open('GET', url)
 xhr.send()
+
+// example main.js for AJAX request to expand a shortened URL (Google URL Shortener API)
+// Include data for accessing Google APIs
+
+const apiKey = 'AIzaSyD1MGM3V3KooE0QZmmzGBK6uzfxVn5zt2c';
+
+const url = 'https://www.googleapis.com/urlshortener/v1/url'
+// Some page elements
+
+const $inputField = $('#input');
+const $expandButton = $('#expand');
+const $shortenButton = $('#shorten');
+const $responseField = $('#responseField');
+
+// AJAX functions
+
+function expandUrl() {
+	const urlToExpand = url + '?shortUrl=' + $inputField.val() + '&key=' + apiKey;
+  const xhr = new XMLHttpRequest()
+  xhr.responseType = 'json'
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+    $responseField.append('<p>Your expanded url is: </p><p>' + xhr.response.longUrl + '</p>');
+		}
+  }
+  xhr.open('GET', urlToExpand)
+  xhr.send()
+}
+
+function shortenUrl() {
+
+}
+
+function expand() {
+  $responseField.empty();
+  expandUrl();
+  return false;
+}
+
+function shorten() {
+  $responseField.empty();
+  shortenUrl();
+  return false;
+}
+
+// Call functions on submit
+
+$expandButton.click(expand);
+$shortenButton.click(shorten);
