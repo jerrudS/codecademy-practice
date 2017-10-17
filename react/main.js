d2cebeb247d5e1e84b0f3244c3847c6aa811ba8c
Jerrud Sabourin <jsabourin1235@gmail.com>
2017-10-17 16:23:38 -0700

    Map and keys.

--- react/main.js
+++ react/main.js
@@ -62,3 +62,14 @@ ReactDOM.render(
 	favoriteFoods,
 	document.getElementById('app')
 );
+
+// map and keys
+const people = ['Rowe', 'Prevost', 'Gare'];
+
+const peopleLis = people.map((person, i) =>
+  // expression goes here:
+	<li key={'person_' + i}>{person}</li>
+);
+
+// ReactDOM.render goes here:
+ReactDOM.render(<ul>{peopleLis}</ul>, document.getElementById('app'));
