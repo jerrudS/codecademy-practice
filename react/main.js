1f14e2b5a23f00d5a261ff69c45f2d9520f8492b
Jerrud Sabourin <jsabourin1235@gmail.com>
2017-10-17 15:47:44 -0700

    Using if/else conditionals.

--- react/main.js
+++ react/main.js
@@ -16,3 +16,25 @@ const kitty = (
 );
 
 ReactDOM.render(kitty, document.getElementById('app'));
+
+// using if/else statements in react
+function coinToss() {
+  // This function will randomly return either 'heads' or 'tails'.
+  return Math.random() < 0.5 ? 'heads' : 'tails';
+}
+
+const pics = {
+  kitty: 'https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-kitty.jpg',
+  doggy: 'https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-puppy.jpeg'
+};
+let img;
+
+// if/else statement begins here:
+if (coinToss() === 'heads') {
+  img = <img src={pics.kitty} />
+}
+else {
+  img = <img src={pics.doggy} />
+}
+
+ReactDOM.render(img, document.getElementById('app'))
