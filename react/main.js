fce7d3a8b85e87306f827ac8b92a12d275d95a0e
Jerrud Sabourin <jsabourin1235@gmail.com>
2017-10-18 12:33:43 -0700

    Conditionals in a render function.

--- react/main.js
+++ react/main.js
@@ -71,5 +71,41 @@ const peopleLis = people.map((person, i) =>
 	<li key={'person_' + i}>{person}</li>
 );
 
-// ReactDOM.render goes here:
 ReactDOM.render(<ul>{peopleLis}</ul>, document.getElementById('app'));
+
+// using a conditional in a render function
+class TodaysPlan extends React.Component {
+  render() {
+    let task;
+    if (!apocalypse) {
+      task = 'learn React.js'
+    } else {
+      task = 'run around'
+    }
+
+    return <h1>Today I am going to {task}!</h1>;
+  }
+}
+
+ReactDOM.render(
+	<TodaysPlan />,
+	document.getElementById('app')
+);
+
+// or
+const fiftyFifty = Math.random() < 0.5;
+
+class TonightsPlan extends React.Component {
+  render() {
+    if (fiftyFifty) {
+      return <h1>Tonight I'm going out WOOO</h1>;
+    } else {
+      return <h1>Tonight I'm going to bed WOOO</h1>;
+    }
+  }
+}
+
+ReactDOM.render(
+	<TonightsPlan />,
+	document.getElementById('app')
+);
